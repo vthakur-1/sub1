0a6e076c7ffdb3194622c1dc4083b48a7f50b94c
vivek <vthakur@bestpeers.com>
2020-01-22 18:54:07 +0530

    Solution by vivek.

---
+++ prob5.py
@@ -0,0 +1,9 @@
+class str():
+	def getstring(self):
+		x=input()
+		return x
+	def upperq(self):
+		print(self.getstring().upper())
+ob=str()
+ob.upperq()		
+		                                                     	
